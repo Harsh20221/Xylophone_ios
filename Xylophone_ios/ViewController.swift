c0a960920a0c48e6b97d942eed832161bc5a999a
Harsh <harsh20221@gmail.com>
2024-07-17 03:58:56 +0530

    Added First Sound

--- Xylophone_ios/ViewController.swift
+++ Xylophone_ios/ViewController.swift
@@ -11,14 +11,31 @@ import AVFoundation
 
 class ViewController: UIViewController {
     var player : AVAudioPlayer? // This is how we assign a Audio player for AV 
+    
+    func playSound() {
+        guard let path = Bundle.main.path(forResource: "C", ofType:"wav") else {
+            return }
+        let url = URL(fileURLWithPath: path)
 
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
     @IBAction func FirstButtonController_C(_ sender: UIButton) {
-        print("I Love You ")
+
         ////Write your Code here for The Action you wanna perform with this Button
+        playSound()
     }
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    
 
 
     
