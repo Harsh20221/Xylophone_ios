8071a05d4db8c067281eedf8d27a06cc6cafd3d3
Harsh <harsh20221@gmail.com>
2024-07-17 04:58:44 +0530

    Reduced code for sound

--- Xylophone_ios/ViewController.swift
+++ Xylophone_ios/ViewController.swift
@@ -14,12 +14,10 @@ class ViewController: UIViewController {
     
     var player : AVAudioPlayer? // This is how we assign a Audio player for AV
     
-    func playSound() {  /////PLAYSOUND FOR C
-    let path = Bundle.main.url(forResource: "C", withExtension:"wav") //This is the updated Player the first line is locating the sound the 2 line is inserting the file into the player and 3 line is actually responsible for playing the sound 
+    func playSoundC() {  /////PLAYSOUND FOR C
+    let path = Bundle.main.url(forResource: "C", withExtension:"wav") //This is the updated Player the first line is locating the sound the 2 line is inserting the file into the player and 3 line is actually responsible for playing the sound
             player = try!AVAudioPlayer(contentsOf: path!)
             player?.play()
-            
-    
     }
     
     
@@ -30,7 +28,7 @@ class ViewController: UIViewController {
     @IBAction func FirstButtonController_C(_ sender: UIButton) { //C-BUTTON
 
         ////Write your Code here for The Action you wanna perform with this Button
-        playSound() //? To add the sound folder in X code , Just Drag and drop the folder in X code , do not use Finder to copy paste   This will play C sound
+        playSoundC() //? To add the sound folder in X code , Just Drag and drop the folder in X code , do not use Finder to copy paste   This will play C sound
     }
     
     
