284563781f1a8a46f2c5cdef6bbc8a2bd3e05457
Harsh <harsh20221@gmail.com>
2024-07-17 05:49:03 +0530

    Added Timed Opacity

--- Xylophone_ios/ViewController.swift
+++ Xylophone_ios/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
     
     func playSound(soundname:String ) {  /////PLAYSOUND
     let path = Bundle.main.url(forResource: soundname, withExtension:"wav") //This is the updated Player the first line is locating the sound the 2 line is inserting the file into the player and 3 line is actually responsible for playing the sound
-        ///!! Make sure to also specify the soundname parameter here in this path variable 
+        ///!! Make sure to also specify the soundname parameter here in this path variable
             player = try!AVAudioPlayer(contentsOf: path!)
             player?.play()
     }
@@ -31,7 +31,12 @@ class ViewController: UIViewController {
         ////Write your Code here for The Action you wanna perform with this Button
         playSound(soundname:sender.currentTitle! ) //? To add the sound folder in X code , Just Drag and drop the folder in X code , do not use Finder to copy paste   This will play C sound
         ///!! Do not forget to add ! after CurrentTitle
-        print(sender.currentTitle!)//? Use this current title to get the title of the Button Pressed
+                print(sender.currentTitle!)//? Use this current title to get the title of the Button Pressed
+        sender.alpha=0.5;
+        let seconds = 0.2  ///The Below code will change the opacity of the button back to original after 0.2 seconds
+        DispatchQueue.main.asyncAfter(deadline: .now() + seconds) {
+            sender.alpha=1;
+        }
     
     }
     
