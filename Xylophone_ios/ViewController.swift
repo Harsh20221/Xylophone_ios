4797a2ff1deeb7aecb97482dc5b0eee3ecd08522
Harsh <harsh20221@gmail.com>
2024-07-17 05:39:11 +0530

    Xylophone Project Completed

--- Xylophone_ios/ViewController.swift
+++ Xylophone_ios/ViewController.swift
@@ -14,8 +14,8 @@ class ViewController: UIViewController {
     
     var player : AVAudioPlayer? // This is how we assign a Audio player for AV
     
-    func playSoundC() {  /////PLAYSOUND FOR C
-    let path = Bundle.main.url(forResource: "C", withExtension:"wav") //This is the updated Player the first line is locating the sound the 2 line is inserting the file into the player and 3 line is actually responsible for playing the sound
+    func playSound(soundname:String ) {  /////PLAYSOUND
+    let path = Bundle.main.url(forResource: soundname, withExtension:"wav") //This is the updated Player the first line is locating the sound the 2 line is inserting the file into the player and 3 line is actually responsible for playing the sound
             player = try!AVAudioPlayer(contentsOf: path!)
             player?.play()
     }
@@ -28,8 +28,8 @@ class ViewController: UIViewController {
     @IBAction func FirstButtonController_C(_ sender: UIButton) { //C-BUTTON
 
         ////Write your Code here for The Action you wanna perform with this Button
-        playSoundC() //? To add the sound folder in X code , Just Drag and drop the folder in X code , do not use Finder to copy paste   This will play C sound
-        print(sender.currentTitle!)//? Use this current title to get the title of the Button Pressed 
+        playSound(soundname:sender.currentTitle! ) //? To add the sound folder in X code , Just Drag and drop the folder in X code , do not use Finder to copy paste   This will play C sound
+        print(sender.currentTitle!)//? Use this current title to get the title of the Button Pressed
     
     }
     
